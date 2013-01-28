9de88b76cd4ef36592509f08e69927807f29f505
Psyfear <psyfear@mac.com>
2013-01-28 15:04:19 +0800

    Update app.rb

--- app.rb
+++ app.rb
@@ -12,9 +12,6 @@ get '/' do
   url = "http://www2.adb.org/Projects/summaries.asp?browse=1"
   data = Nokogiri::HTML(open(url))
 
-  # this line has only be adjusted slightly with the inclusion of an ampersand
-  # before concerts.  This creates an instance variable that can be referenced
-  # in our display logic (view).
   @projects = data.css('.e')
 
 
